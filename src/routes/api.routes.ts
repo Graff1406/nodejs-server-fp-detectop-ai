caf0dc266e97a2b8e9cc3b4f79cd8c761d187d49
avtandil <shainidze@gmail.com>
2025-02-02 10:42:50 +0200

    feat: add api reg handler for analyze

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -2,6 +2,15 @@ import { Router } from 'express';
 
 const router = Router();
 
-// router.get('/analyze', () => {}); // api/analyze
+router.get('/analyze', (req, res) => {
+  const { url, lang } = req.query;
+
+  res.json({
+    message: 'Data received',
+    analyzedUrl: url,
+    detectedLanguage: lang,
+    /* Analysis data will be here */
+  });
+}); // api/analyze
 
 export { router };
